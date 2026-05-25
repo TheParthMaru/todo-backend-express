bee481036c64141b68f87238b58c4549775975ea
Parth Maru <theparthmaru@gmail.com>
2026-05-25 14:39:54 +0100

    add a simple / endpoint to check if everything is working fine

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const { PORT } = require("./config/server.config");
 
 const app = express();
+
+app.get("/", (req, res) => {
+    return res.json({ msg: "ok" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+});
